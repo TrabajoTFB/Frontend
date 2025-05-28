e6a76f26faac798af7b1cdf6f0ec137bcf97094b
Pablo <pabmengibar@gmail.com>
2025-05-28 09:34:57 +0200

    responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="profile" element={<Profile />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </main>
         <Footer />
